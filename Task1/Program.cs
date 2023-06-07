ca74ced956966b7c692133d46069adba23d64521
Veronika <16nika94@mail.ru>
2023-06-07 21:46:47 +0300

    HomeWork Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] array2d = new double [3,4];
+for (int i=0; i<array2d.GetLength(0); i++)
+{
+    for(int j=0; j<array2d.GetLength(1); j++)
+    {
+       array2d [i,j] = new Random(). Next (-100,100) / 10.0;
+        Console.Write( array2d[i,j] + " ");
+    }
+    Console.WriteLine( );
+}
