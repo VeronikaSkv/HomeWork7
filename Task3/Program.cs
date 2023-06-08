f07e69896781656acc24ddaf0258b16efdd3658d
Veronika <16nika94@mail.ru>
2023-06-08 21:24:01 +0300

    Homework task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,3 +6,28 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+int sum0= 0;
+double Sum=0;
+int [,] array2d = new int [3,4];
+for (int i=0; i<array2d.GetLength(0); i++)
+{
+    for(int j=0; j<array2d.GetLength(1); j++)
+    {
+       array2d [i,j] = new Random(). Next (1,10);
+        Console.Write( array2d[i,j] + " ");
+           }
+    Console.WriteLine( );
+}
+
+Console.Write($"Среднее арифметическое каждого столбца: ");
+
+for (int j=0; j<array2d.GetLength(1); j++)
+{
+    for(int i=0; i<array2d.GetLength(0); i++)
+    {
+       sum0 = sum0+ array2d [i,j];
+           }
+           Sum=sum0*10/3 /10.0;
+           sum0=0;
+    Console.Write($"{Sum}; ");
+}
